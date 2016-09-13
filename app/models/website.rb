8fafcc6c74aaa084e5ce891542f1b63ac7b12d6a
pachanga <fatin.d.cooper@students.coookman.edu>
2016-09-13 17:28:13 +0000

    spelling again

--- app/models/website.rb
+++ app/models/website.rb
@@ -4,7 +4,8 @@ class Website < ActiveRecord::Base
 
     def headline
         
-      
+      puts h2_scan
+      return h2_scan
     
     end
     
@@ -20,10 +21,10 @@ class Website < ActiveRecord::Base
        # method is_TAG
        # tag = is_TAG(look)
        # method is_WORD
-       # method is_GIBERISH
+       # method is_GIBERISH.
        # if tag == true
-       # if word == true
-       return # that the indentified checks out
+       # if word == true.
+       return # that the indentified checks out.
     end
 
     def is_WORD
@@ -52,10 +53,10 @@ class Website < ActiveRecord::Base
                 #p "#{name} , #{webiste} , #{identifier}"
                 file = open("http://#{name}")
                 content = file.read
-                unless content.index("<h2").nil?
+                unless content.index("<h1").nil?
                 #start_location = content.index(identifier)
-                header_start = content.index("<h2") # + 4
-                header_end = content.index("</h2>") # - 1
+                header_start = content.index("<h1") # + 4
+                header_end = content.index("</h1>") # - 1
                 p content[header_start..header_end]
                     #p "There is no h1 tag on {#name}"
                 end
@@ -88,11 +89,11 @@ class Website < ActiveRecord::Base
             #p "#{name} , #{webiste} , #{identifier}"
             file = open("http://#{name}")
             content = file.read
-            unless content.index("<h2").nil?
+            unless content.index("<h1").nil?
             
             #start_location = content.index(identifier)
-            header_start = content.index("<h2") # + 4
-            header_end = content.index("</h2>") # - 1
+            header_start = content.index("<h1") # + 4
+            header_end = content.index("</h1>") # - 1
             p content[header_start..header_end]
                 #p "There is no h1 tag on {#name}"
             end
@@ -107,10 +108,10 @@ class Website < ActiveRecord::Base
             #p "#{name} , #{webiste} , #{identifier}"
             file = open("http://#{name}")
             content = file.read
-            unless content.index("<h2").nil?
+            unless content.index("<h1").nil?
             #start_location = content.index(identifier)
-            header_start = content.index("<h2") # + 4
-            header_end = content.index("</h2>") # - 1
+            header_start = content.index("<h1") # + 4
+            header_end = content.index("</h1>") # - 1
             p content[header_start..header_end]
                 #p "There is no h1 tag on {#name}"
             end
